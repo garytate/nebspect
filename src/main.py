166f2363ff08570b3b795d695f31b3a4a24ff63c
Gary Tate <me@garytate.co.uk>
2019-10-08 19:44:59 +0100

    General updates

--- src/main.py
+++ src/main.py
@@ -3,17 +3,20 @@
 ## Version 0.0.1 published 08/10/19
 
 ## Imports
-import sys
 import os
-from lib import directory
+from lib import directory, characters
 
 ## Initiating
 os.system('cls')
 print("Nebspect: Version 0.0.1 - Last updated 08/10/2019\n")
 
+# Initaliase sub-directories
 main_folder = os.path.dirname(os.path.abspath(__file__))
 log_folder = directory.getChildDirectory(main_folder, "\logs")
 char_folder = directory.getChildDirectory(main_folder, "\characters")
 
-print(main_folder, log_folder, char_folder)
+characters = characters.loadCharacters(char_folder)
+
+for char in characters:
+    print(char)
 input()
