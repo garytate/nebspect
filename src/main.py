3de9aa0e65cbb6bba1be0a955660d0818655ac2e
Gary Tate <me@garytate.co.uk>
2019-10-09 03:35:38 +0100

    Added basic logging implementation

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 
 ## Imports
 import os, sys
-from lib import directory, characters
+from lib import directory, characters, logging
 
 ## Initiating
 os.system('cls')
@@ -15,8 +15,12 @@ main_folder = os.path.dirname(os.path.abspath(__file__))
 log_folder = directory.getChildDirectory(main_folder, "/logs")
 char_folder = directory.getChildDirectory(main_folder, "/characters")
 
+# Loading all characters into memory
 characters = characters.loadCharacters(char_folder)
+
+# Loading all log file names into memory
+logArray = logging.getLogArray(log_folder)
+print(logArray)
 
 for char in characters:
     print(char)
-input()
